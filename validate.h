ede791285338a787e0dff5d5a15c31463884dcdb
Durlug <shaun.g.richardson@gmail.com>
2016-12-01 22:57:00 -0500

    Add files via upload

--- validate.h
+++ validate.h
@@ -3,28 +3,22 @@ template <class A, class B, class C, class D, class E, int length>
 
 bool validate(const A& min, const B& max, C (&array)[length], D size, E e[])
 {
-	bool result;
+	bool result = true;
 
 	for (int i = 0; i <= size; i++)
 	{
 		if (min > array[i] || max < array[i])   //Checking to see if value is outside min/max range
 		{
 			result = false;                    //Sets result to false if it is
-			e[i] = result;                     //Sets bool array index to false 
+			e[i] = false;                     //Sets bool array index to false 
 		}
 		else
 		{
-			result = true;                    //If within range, result is set to true
-			e[i] = result;                    //Sets bool array index to true
+			e[i] = true;                    //Sets bool array index to true
 		}
-
-		if (e[i] == false)
-		{
-			return false;
-		}
-		else
-			return true;
 	}
+
+	return result;
 }
 
 
